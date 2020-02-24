03ac6058ade59415721e8a30d09c48c1050b1d40
Yeeons <yeeons@gmail.com>
2020-02-25 10:44:11 +1100

    Updated to version 2

--- Parachute_1.0.jsx
+++ Parachute_1.0.jsx
@@ -1,69 +1,208 @@
+/////// ----------- update these values ----------- ///////
+var em = 16
+var masterFont = "Arial"
+
+
+// Folder name
+var groupName = "Parachute";
+var characterGroupName = "Charachute";
+
+
+////// ****** Master Variables ***** //////
+///// Para attributes /////
+var masterBase = {
+    name: "$ master",
+    appliedFont: masterFont,
+    hyphenation: false,
+    kerningMethod: "Optical",
+    ligatures: false,
+    spaceAfter: em * 1 + "pt"
+}
+
+var hBase = {
+    name: "$ h-master",
+    appliedFont: masterFont,
+    fontStyle: "Bold"
+}
+
+var pBase = {
+    name: "$ p-master",
+    appliedFont: masterFont,
+    fontStyle: "Regular"
+}
+
+var h1 = {
+    name:  "h1",
+    appliedFont: masterFont,
+    pointSize: em * 2.5
+}
+
+var h2 = {
+    name:  "h2",
+    appliedFont: masterFont,
+    pointSize: em * 2
+}
+
+var h3 = {
+    name:  "h3",
+    appliedFont: masterFont,
+    pointSize: em * 1.75
+}
+
+var h4 = {
+    name:  "h4",
+    appliedFont: masterFont,
+    pointSize: em * 1.5
+}
+
+var h5 = {
+    name:  "h5",
+    appliedFont: masterFont,
+    pointSize: em * 1.25
+}
+
+var h6 = {
+    name:  "h6",
+    appliedFont: masterFont,
+    pointSize: em * 1
+}
+
+var p = {
+    name:  "p",
+    appliedFont: "Arial",
+    pointSize: em * 1,
+}
+
+var p2 = {
+    name:  "p2",
+    appliedFont: "Arial",
+    pointSize: em * 0.6875,
+}
+
+////// ****** Character Styles ***** //////
+
+var bold = {
+    name: "Bold",
+    fontStyle: "Bold"
+}
+
+var italic = {
+    name: "Italic",
+    fontStyle: "Italic"
+}
+
+var superScript = {
+    name: "Superscript",
+    position: 1936749411
+}
+
+var noBreak = {
+    name: "No Break",
+    noBreak: true
+}
+
+////// ****** Main Code ***** //////
 var myDoc = app.documents.item(0);
-var gStyle = myDoc.paragraphStyleGroups.item("Font Name");
+var paraGroups = myDoc.paragraphStyleGroups;
+var charGroups = myDoc.characterStyleGroups;
 
-// Create Group for styles to live in
-try {
-  paraGroupStyle = myDoc.paragraphStyleGroups.item("Font Name");
-  myName = paraGroupStyle.name;
-  alert("Paragraph group already exists");
-  } catch(myError) {
-    paraGroupStyle = myDoc.paragraphStyleGroups.add({name:"Font Name"});
-  }
+var num = 0;
 
-// Paragraph styles
-// Masters
-paraCreator("• Master", "", 12, "None"); //Name, font weight, point size, based on 
-paraCreator("• H Master", "Bold", 12, "• Master");
-paraCreator("• P Master", "Regular", 10, "• Master");
+// gives us the current paragraph group name, so we can add things to it
+var groupNameReturn = groupName;
 
-// Headings
-paraCreator("H1", "Bold", 30, "• H Master");
-paraCreator("H2", "Bold", 16, "• H Master");
-paraCreator("H3", "Bold", 12, "• H Master");
+function createPGroup(){
+    if ( paraGroups.length == 0 ) {
+        paraGroups.add( { name: groupName } );
+        paraGroups[ paraGroups.item( groupNameReturn ).index ].paragraphStyleGroups.add( { name: "Para masters" } );
+    } else {
+        for ( var i = 0; paraGroups.length > i; i++ ){
+            if ( paraGroups[i].name == groupName ){
+                pGroupAdd( num );
+                return;
+            } 
+        }
+        paraGroups.add( { name: groupName } )
+        paraGroups[ paraGroups.item( groupNameReturn ).index ].paragraphStyleGroups.add( { name: "Para masters" } );
+    }
+}
 
-// Paragraphs
-paraCreator("P", "", 10, "• P Master");
-paraCreator("P1", "", 10, "• P Master"); // May look to remove this as copying p would be easier
+function pGroupAdd( num ){
+    for ( var i = 0; paraGroups.length > i; i++ ){
+        if ( paraGroups[i].name === groupName + "_" + num ){
+            num++;
+            pGroupAdd( num );
+            return;
+        }
+    }
+    paraGroups.add( {name: ( groupName + "_" + num ) } );
 
-// Character styles
-// Character
-charCreator("Bold", "Bold");
-charCreator("Italic", "Italic");
-charCreator("Superscript", 2);
+    groupNameReturn = groupName + "_" + num
+    paraGroups[paraGroups.item(groupNameReturn).index].paragraphStyleGroups.add({name: "Para masters"})
+}
 
-function paraCreator(styleName, fontWeight, fontSize, paraBasedOn){
-  try {
-    paraStyle = gStyle.paragraphStyles.item(styleName);
-    myName = paraStyle.name;
-  } catch(myError) {
-    paraStyle = gStyle.paragraphStyles.add({name:styleName});
-  }
+// Character folder create
+function createCGroup(){
+    if ( charGroups.length == 0 ){
+        charGroups.add( { name: characterGroupName } );
+    } else {
+        for ( var i = 0; charGroups.length > i; i++ ){
+            if ( charGroups[i].name == characterGroupName ){
+                return;
+            }
+        }
+        charGroups.add( { name: characterGroupName } );
+    }
+}
 
-  try {
+createCGroup()
+createPGroup()
 
-    paraStyle.basedOn = gStyle.paragraphStyles.item(paraBasedOn);
-  } catch (myError){
-  }
-  
-  try {
-    paraStyle.fontStyle = fontWeight;
-    paraStyle.pointSize = fontSize;
-  } catch(myError){
-  }
+var currentCGroup = charGroups.item( characterGroupName ).characterStyles
+var currentPGroup = paraGroups.item( groupNameReturn ).paragraphStyles;
+var currentsettingGroup = paraGroups.item( groupNameReturn ).paragraphStyleGroups[0].paragraphStyles
 
-};
 
-function charCreator(charName, option){
-  try {
-    charStyle = myDoc.characterStyles.item(charName);
-    myName = charStyle.name;
-  } catch(myError) {
-    charStyle = myDoc.characterStyles.add({name:charName});
-  }
-    
-  if (charName == "Superscript") {
-    charStyle.position = 1936749411;
-  } else {
-    charStyle.fontStyle = option;
-  }
-};
+//////////////// group end
+
+
+function pStyleAdd( properties, folder, currentFolder, basedOnFolder ){
+    folder.add( properties );
+    if ( basedOnFolder != "nill") {
+        if ( currentFolder == "$ h-master" || currentFolder == "$ p-master" ){
+            paraStyle = currentsettingGroup.item( currentFolder );
+            paraStyle.basedOn = currentsettingGroup.item( basedOnFolder );
+        } else {
+            paraStyle = currentPGroup.item( currentFolder );
+            paraStyle.basedOn = currentsettingGroup.item( basedOnFolder );
+        }
+    } else {
+        paraStyle = currentsettingGroup.item( currentFolder );
+    }
+}
+
+function cStyleAdd( properties ){
+    currentCGroup.add( properties )
+}
+
+
+////// ****** Add in para styles here ***** //////
+////// ****** Remove a line if not needed ***** //////
+pStyleAdd( masterBase, currentsettingGroup, "$ master", "nill" );
+pStyleAdd( hBase, currentsettingGroup, "$ h-master", "$ master" );
+pStyleAdd( pBase, currentsettingGroup, "$ p-master", "$ master" );
+
+pStyleAdd( h1, currentPGroup, "h1", "$ h-master" );
+pStyleAdd( h2, currentPGroup, "h2", "$ h-master" );
+pStyleAdd( h3, currentPGroup, "h3", "$ h-master" );
+pStyleAdd( h4, currentPGroup, "h4", "$ h-master" );
+pStyleAdd( h5, currentPGroup, "h5", "$ h-master" );
+pStyleAdd( h6, currentPGroup, "h6", "$ h-master" );
+
+pStyleAdd( p, currentPGroup, "p", "$ p-master" );
+pStyleAdd( p2, currentPGroup, "p2", "$ p-master" );
+
+cStyleAdd( bold );
+cStyleAdd( italic );
+cStyleAdd( superScript );
+cStyleAdd( noBreak );
